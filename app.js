693c4305e7c0d5d34677e1aeb81d5735fbd3930e
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-12-25 19:57:51 -0300

    adding search button and styling

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
-
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 const app = express();
 
+
 const PORT = 5000 || process.env.PORT;
+
 
 // setting public folder
 app.use(express.static("public"));
@@ -15,7 +16,10 @@ app.use(expressLayout);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
+
+
 app.use("/", require("./server/routes/main"));
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port: ${PORT}`);
