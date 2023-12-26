9efffe169bba9f9ee1a98a7ff674533a8c3cdc82
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-12-25 21:09:31 -0300

    connect to mongodb

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+
+const connectDB = require('./server/config/db')
+
 const app = express();
 
 
 const PORT = 5000 || process.env.PORT;
 
+
+// connect db
+connectDB();
 
 // setting public folder
 app.use(express.static("public"));
